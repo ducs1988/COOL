c1ac1abc24e7bb22ca1c5f4936878e5fd0b95d3a
ducs1988 <ducs1988@gmail.com>
2018-07-26 00:23:48 -0500

    LC259
    3Sum Smaller

---
+++ NO1/LC259.java
@@ -0,0 +1,21 @@
+public class Solution {
+	public int threeSumSmaller(int nums, int target) {
+		if (nums == null || nums.length < 3)
+			return 0;
+		Arrays.sort(nums);
+		int count = 0;
+		for (int i = 0; i < nums.length - 2; i++) {
+			int left = i+1, right = nums.length-1;
+			while (left < right) {
+				int sum = nums[i] + nums[left] + nums[right];
+				if (sum < target) {
+					count += (right - left);
+					left++;
+				} else {
+					right--;
+				}
+			}
+		}
+		return count;
+	}
+}
