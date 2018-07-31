b66a9bf362d3e67bae50d06bc5b7b6ef724917ff
vedolos <ducs1988@gmail.com>
2018-07-30 21:58:51 -0500

    LC101
    Symmetric Tree

---
+++ NO1/LC101.java
@@ -0,0 +1,33 @@
+// Symmetric Tree
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null)
+            return true;
+        return helper(root.left, root.right);
+    }
+    
+    private boolean helper(TreeNode left, TreeNode right) {
+        if (left == null && right == null)
+            return true;
+        if (left == null || right == null)
+            return false;
+        if (left.val != right.val)
+            return false;
+        
+        if (!helper(left.left, right.right))
+            return false;
+        if (!helper(left.right, right.left))
+            return false;
+        return true;
+    }
+}
