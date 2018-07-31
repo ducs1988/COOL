466d9366ecfad545d4a4ad2d981261a956ea162b
vedolos <ducs1988@gmail.com>
2018-07-30 22:15:39 -0500

    LC49
    Group Anagrams

---
+++ NO1/LC49.java
@@ -0,0 +1,34 @@
+/*
+Given an array of strings, group anagrams together.
+
+Example:
+
+Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
+Output:
+[
+  ["ate","eat","tea"],
+  ["nat","tan"],
+  ["bat"]
+]
+Note:
+
+All inputs will be in lowercase.
+The order of your output does not matter.
+*/
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs == null || strs.length == 0)
+            return new ArrayList<>();
+        Map<String, List<String>> map = new HashMap<>();
+        for (String s : strs) {
+            char[] chs = s.toCharArray();
+            Arrays.sort(chs);
+            String key = new String(chs);
+            if (!map.containsKey(key))
+                map.put(key, new ArrayList());
+            map.get(key).add(s);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
