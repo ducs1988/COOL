62f57389bb58418df59084edd8b39971782541ea
vedolos <ducs1988@gmail.com>
2018-08-04 20:00:17 -0500

    LC217
    Contains Duplicate

---
+++ NO1/LC217.java
@@ -0,0 +1,14 @@
+// contains duplicate
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        if (nums == null || nums.length == 0)
+            return false;
+        Set<Integer> set = new HashSet<>();
+        for (int n : nums) {
+            if (set.contains(n))    return true;
+            set.add(n);
+        }
+        return false;
+    }
+}
