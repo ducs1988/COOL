e4f83a6a0799147594c3a5c077b779af8186bc0f
vedolos <ducs1988@gmail.com>
2018-08-05 23:03:01 -0500

    LC 167

---
+++ NO1/LC167.java
@@ -0,0 +1,22 @@
+/**
+ * Tow sum II - Input array is sorted
+ */
+
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int low = 0, high = numbers.length - 1;
+        int[] ans = new int[2];
+        
+        while (low < high) {
+            int sum = numbers[low] + numbers[high];
+            if (sum == target) {
+                ans[0] = low+1;
+                ans[1] = high+1;
+                break;
+            }
+            if (sum < target)   low++;
+            else    high--;
+        }
+        return ans;
+    }
+}
