2233749af34231dcf352882e2b168474e036ab7e
vedolos <ducs1988@gmail.com>
2018-09-10 22:03:34 -0500

    LC896 [Array]

---
+++ NO1/LC896.java
@@ -0,0 +1,27 @@
+/**
+ * Monotonic Array
+ * 
+ * An array is monotonic if it is either monotone increasing or monotone decreasing.
+
+An array A is monotone increasing if for all i <= j, A[i] <= A[j].  An array A is monotone decreasing if for all i <= j, A[i] >= A[j].
+
+Return true if and only if the given array A is monotonic.
+ */
+
+Class Solution {
+    public boolean isMonotonic(int[] A) {
+        if (A == null || A.length == 1)
+            return true;
+
+        int store = 0;
+        for (int i = 0; i < A.length-1; i++) {
+            int c = Integer.compare(A[i], A[i+1]);
+            if (c != 0) {
+                if (c != store && store != 0)
+                    return false;
+                store = c;
+            }
+        }
+        return true;
+    }
+}
