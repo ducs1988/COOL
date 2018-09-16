56beb242e1cfeeb49c85c8d301f40bd603a042f1
vedolos <ducs1988@gmail.com>
2018-09-16 17:10:10 -0500

    LC320

---
+++ NO1/LC320.java
@@ -0,0 +1,41 @@
+/**
+ * Generalized Abbreviation
+ * 
+ * Write a function to generate the generalized abbreviations of a word. 
+
+Note: The order of the output does not matter.
+
+Example:
+
+Input: "word"
+Output:
+["word", "1ord", "w1rd", "wo1d", "wor1", "2rd", "w2d", "wo2", "1o1d", "1or1", "w1r1", "1o2", "2r1", "3d", "w3", "4"]
+ 
+ */
+
+public class Solution {
+    public List<String> generateAbbreviations(String word){
+        List<String> ans = new ArrayList<String>();
+        backtrack(ans, new StringBuilder(), word, 0, 0);
+        return ans;
+    }
+
+    // i is the current position
+    // k is the count of consecutive abbreviated characters
+    private void backtrack(List<String> ans, StringBuilder builder, String word, int i, int k){
+        int len = builder.length(); // keep the length of builder
+        if(i == word.length()){
+            if (k != 0) builder.append(k); // append the last k if non zero
+            ans.add(builder.toString());
+        } else {
+            // the branch that word.charAt(i) is abbreviated
+            backtrack(ans, builder, word, i + 1, k + 1);
+
+            // the branch that word.charAt(i) is kept
+            if (k != 0) builder.append(k);
+            builder.append(word.charAt(i));
+            backtrack(ans, builder, word, i + 1, 0);
+        }
+        builder.setLength(len); // reset builder to the original state
+    }
+}
