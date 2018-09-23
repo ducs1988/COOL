494a87fdf7e1a692ad0f2d6a5fd4d74e3417c001
vedolos <ducs1988@gmail.com>
2018-09-23 18:40:21 -0500

    LC199

---
+++ NO1/LC199.java
@@ -0,0 +1,46 @@
+import java.util.Queue;
+
+import javax.swing.tree.TreeNode;
+
+/**
+ * Binary Tree Right Side View
+ * 
+ */
+
+ /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if (root == null)
+            return list;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+
+            for (int i = 0; i < size; i++) {
+                TreeNode right = queue.poll();
+                if (i = 0) {
+                    list.add(right.val);
+                }
+                if (right.right != null) {
+                    queue.add(right.right);
+                }
+                if (right.left != null) {
+                    queue.add(right.left);
+                }
+            }
+        }
+        return list;
+    }
+}
