e3c4bae95791369fccf877366b095a48868edd4f
vedolos <ducs1988@gmail.com>
2018-11-01 22:45:58 -0500

    LC103

---
+++ NO1/LC103.java
@@ -0,0 +1,53 @@
+/**
+ * Given a binary tree, return the zigzag level order traversal of its nodes' values. (ie, from left to right, then right to left for the next level and alternate between).
+
+For example:
+Given binary tree [3,9,20,null,null,15,7],
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its zigzag level order traversal as:
+[
+  [3],
+  [20,9],
+  [15,7]
+]
+ */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null)
+            return res;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < size; ++i) {
+                TreeNode tmp = queue.poll();
+                list.add(tmp.val);
+                
+                if (tmp.left != null)
+                    queue.offer(tmp.left);
+                if (tmp.right != null)
+                    queue.offer(tmp.right);
+            }
+            if (res.size() % 2 == 1)
+                Collections.reverse(list);
+            res.add(list);
+        }
+        return res;
+    }
+}
