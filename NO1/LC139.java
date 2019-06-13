358ed6627bcf9e71b5bb0f1d82962e0b742122ed
vedolos <ducs1988@gmail.com>
2019-06-13 00:43:30 -0500

    DP 139

--- NO1/LC139.java
+++ NO1/LC139.java
@@ -46,3 +46,20 @@ class Solution {
         return false;
     }
 }
+
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        boolean[] dp = new boolean[s.length() + 1];
+        Set<String> set = new HashSet(wordDict);
+        dp[0] = true;
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && set.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[s.length()];
+    }
+}
