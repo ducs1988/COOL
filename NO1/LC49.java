774e5a93397bfd7d5b7efac61d58a69b0f06b5b5
vedolos <ducs1988@gmail.com>
2019-07-09 23:55:21 -0500

    up 49

--- NO1/LC49.java
+++ NO1/LC49.java
@@ -18,17 +18,26 @@ The order of your output does not matter.
 
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> res = new ArrayList<>();
         if (strs == null || strs.length == 0)
-            return new ArrayList<>();
+            return res;
         Map<String, List<String>> map = new HashMap<>();
+        int[] count = new int[26];
         for (String s : strs) {
-            char[] chs = s.toCharArray();
-            Arrays.sort(chs);
-            String key = new String(chs);
+            Arrays.fill(count, 0);
+            for (char c : s.toCharArray())
+                count[c - 'a']++;
+            StringBuilder sb = new StringBuilder();
+            for (int num : count)
+                sb.append("#").append(num);
+            String key = sb.toString();
             if (!map.containsKey(key))
-                map.put(key, new ArrayList());
+                map.put(key, new ArrayList<String>());
             map.get(key).add(s);
         }
-        return new ArrayList<>(map.values());
+        return new ArrayList(map.values());
     }
 }
+
+// Time O(NK) where N is the length of strs, K is max length of a string in strs
+// Space O(NK)
