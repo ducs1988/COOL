1e112de5ba5d8dbe23a0fbcdeeb2c8c7e4117b08
vedolos <ducs1988@gmail.com>
2019-07-13 16:36:31 -0500

    up 200

--- NO1/LC200.java
+++ NO1/LC200.java
@@ -20,10 +20,7 @@ class Solution {
     }
 
     private void remove(char[][] grid, int i, int j) {
-        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length)
-            return;
-
-        if (grid[i][j] == '0')
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0')
             return;
 
         grid[i][j] = '0';
