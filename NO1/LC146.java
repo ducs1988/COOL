6c1ae806e14a4fda98a29586dedc6c2989b27df4
vedolos <ducs1988@gmail.com>
2019-07-29 00:14:59 -0500

    up 146

--- NO1/LC146.java
+++ NO1/LC146.java
@@ -1,78 +1,68 @@
 class LRUCache {
-    class ListNode {
-        public int key, val;
-        public ListNode prev, next;
-        
-        public ListNode(int key, int val) {
-            this.key = key;
-            this.val = val;
-            this.next = null;
-            this.prev = null;
+    
+    class Node {
+        int key, val;
+        Node prev, next;
+        public Node(int k, int v) {
+            this.key = k;
+            this.val = v;
+            prev = null;
+            next = null;
         }
     }
-    
-    private int size, capacity;
-    private ListNode dummy, tail;
-    private Map<Integer, ListNode> map;
 
+    int capacity;
+    Map<Integer, Node> map; // key, node
+    Node dummy, tail;
+    
     public LRUCache(int capacity) {
         this.capacity = capacity;
-        this.size = 0;
-        this.dummy = new ListNode(0, 0);
-        this.tail = this.dummy;
-        this.map = new HashMap<Integer, ListNode>();
+        this.map = new HashMap<>();
+        this.dummy = new Node(0,0);
+        tail = dummy;
     }
     
     public int get(int key) {
         if (!map.containsKey(key))
             return -1;
-        
-        move2least(key);
-        return tail.val;
+        Node node = map.get(key);
+        moveToTail(node);
+        return node.val;
     }
     
     public void put(int key, int value) {
-        if (this.get(key) != -1) {
-            ListNode node = map.get(key);
-            node.val = value;
+        if (map.containsKey(key)) {
+            map.get(key).val = value;
+            moveToTail(map.get(key));
             return;
         }
         
-        if (size < capacity) {
-            size++;
-            ListNode node = new ListNode(key, value);
-            tail.next = node;
-            node.prev = tail;
-            tail = node;
-            map.put(key, tail);
-            return;
+        Node node = new Node(key, value);
+        tail.next = node;
+        node.prev = tail;
+        tail = node;
+        map.put(key, node);
+        
+        if (map.size() > capacity) {
+            Node remove = dummy.next;
+            map.remove(remove.key);
+            Node next = remove.next;
+            dummy.next = next;
+            next.prev = dummy;
         }
-        
-        // if reach to the max capacity
-        ListNode last = dummy.next;
-        map.remove(last.key);
-        
-        last.key = key;
-        last.val = value;
-        
-        map.put(key, last);
-        move2least(key);
     }
     
-    private void move2least(int key) {
-        ListNode curt = map.get(key);
-        ListNode prev = curt.prev;
-        ListNode next = curt.next;
-        if (next == null)
-            return;
-        
-        prev.next = next;
-        next.prev = prev;
-        
-        tail.next = curt;
-        curt.prev = tail;
-        curt.next = null;
-        tail = curt;
+    private void moveToTail(Node node) {
+        if (node.next != null) {
+            Node prev = node.prev;
+            Node next = node.next;
+            prev.next = next;
+            next.prev = prev;
+            tail.next = node;
+            node.prev = tail;
+            node.next = null;
+            tail = node;
+        }
     }
 }
 
